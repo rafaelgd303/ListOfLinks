b59433a1348cb99afb62a754c55afb74fb12fc36
Rafael <rafal.pawlowski11@gmail.com>
2018-03-20 21:19:39 +0100

    added hashids

--- Repository/LinksRepository.cs
+++ Repository/LinksRepository.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using webdev.Interfaces;
 using webdev.Models;
+using HashidsNet;
 
 namespace webdev.Repository
 {
@@ -11,15 +12,19 @@ namespace webdev.Repository
 
         public LinksRepository()
         {
-            _links = new List<Link>
-            {
-                new Link { Id = 0, FullLink = "Lód", ShortLink = "Jacek Dukaj" },
-                new Link { Id = 1, FullLink = "Valis", ShortLink = "Philip K. Dick" }
-            };
+            _links = new List<Link>();
         }
 
         public void AddLink(Link link) 
         {
+            // Hashids someHash = new Hashids(full);
+            // var id = full.Encode(1,2,3);
+            // link.ShortLink = id;
+            var hashids = new Hashids(link.FullLink);
+            var id = hashids.Encode(1, 2, 3);
+            var numbers = hashids.Decode(id);
+
+            link.ShortLink = id;
             link.Id = _links.Count;
             _links.Add(link);
         }
@@ -40,7 +45,15 @@ namespace webdev.Repository
         {
             var linkToUpdateIndex = _links.FindIndex(element => element.Id == link.Id);
             if(linkToUpdateIndex != -1) 
+            {
+                var hashids = new Hashids(link.FullLink);
+                var id = hashids.Encode(1, 2, 3);
+                var numbers = hashids.Decode(id);
+                link.ShortLink = id;
+
                 _links[linkToUpdateIndex] = link;
+
+            }
         }
     }
 }
